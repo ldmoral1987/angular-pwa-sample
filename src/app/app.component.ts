c92b02dcc260779b97a4391a9b7a8939d624fc2d
Luis del Moral Martínez <ldmoral1987@gmail.com>
2021-03-12 09:29:28 +0100

    Se crea la vista app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { RestApiService } from './rest-api.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+
+// Esta interfaz contiene los atributos de la tabla que va a mostrarse por pantalla
+export interface TableElement {
+  id: string;
+  name: string;
+  email: string;
+  website: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// Se importa el servicio RestApiService para recuperar y mostrar los datos de los usuarios.
+// Adicionalmente, se usa Material para mostrar los datos con Material table UI.
+// Se controla el número de items con el módulo de paginación de Material
+
 export class AppComponent {
-  title = 'angular-pwa-sample';
+  Data: TableElement[] = null;
+  col: string[] = ['id', 'name', 'email', 'website'];
+  dataSource = new MatTableDataSource<TableElement>(this.Data);
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  constructor(private restApiService: RestApiService) {
+    // Nos suscribimos al servicio para obtener los usuarios
+    this.restApiService.getUsers().subscribe((res) => {
+      this.dataSource = new MatTableDataSource<TableElement>(res);
+      setTimeout(() => {
+        this.dataSource.paginator = this.paginator;
+      }, 0);
+    })
+  }
+
 }
--- src/app/rest-api.service.ts
+++ src/app/rest-api.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+// Esta interfaz contiene los campos del modelo de datos que gestiona el servicio
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  website: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class RestApiService {
 
-  constructor() { }
+export class RestApiService {
+  // Esta string contiene la URL del servicio de usuarios a consumir (jsonplaceholder)
+  api: string = "https://jsonplaceholder.typicode.com/users";
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    // Se devuelven los usuarios de la API como un observable del tipo User
+    return this.http.get<User[]>(this.api)
+  }
 }
