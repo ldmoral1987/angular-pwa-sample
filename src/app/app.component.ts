ff07322ff595cb514e62c81fa23922e5b04a12e9
Luis del Moral Martínez <ldmoral1987@gmail.com>
2021-03-12 09:50:30 +0100

    Se reconfigura el servicio para consumir una api de post, y se adapta la vista y el resto de componentes a la informacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,21 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 
 // Esta interfaz contiene los atributos de la tabla que va a mostrarse por pantalla
+
+// TableElement para la API de usuarios (User)
+//export interface TableElement {
+//  id: string;
+//  name: string;
+//  email: string;
+//  website: string;
+//}
+
+// TableElement para la API de posts (Post)
 export interface TableElement {
-  id: string;
-  name: string;
-  email: string;
-  website: string;
+  userId: string;
+  id: number;
+  title: string;
+  body: string;
 }
 
 @Component({
@@ -23,13 +33,15 @@ export interface TableElement {
 
 export class AppComponent {
   Data: TableElement[] = null;
-  col: string[] = ['id', 'name', 'email', 'website'];
+  //col: string[] = ['id', 'name', 'email', 'website'];
+  col: string[] = ['userId', 'id', 'title', 'body'];
+
   dataSource = new MatTableDataSource<TableElement>(this.Data);
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   constructor(private restApiService: RestApiService) {
     // Nos suscribimos al servicio para obtener los usuarios
-    this.restApiService.getUsers().subscribe((res) => {
+    this.restApiService.getPosts().subscribe((res) => {
       this.dataSource = new MatTableDataSource<TableElement>(res);
       setTimeout(() => {
         this.dataSource.paginator = this.paginator;
--- src/app/rest-api.service.ts
+++ src/app/rest-api.service.ts
@@ -3,11 +3,19 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 // Esta interfaz contiene los campos del modelo de datos que gestiona el servicio
+// Se crean las interfaces User y Post, para probar diferentes fuentes de datos de la API
 export interface User {
   id: string;
   name: string;
   email: string;
   website: string;
+}
+
+export interface Post {
+  userId: string;
+  id: number;
+  title: string;
+  body: string;
 }
 
 @Injectable({
@@ -16,12 +24,18 @@ export interface User {
 
 export class RestApiService {
   // Esta string contiene la URL del servicio de usuarios a consumir (jsonplaceholder)
-  api: string = "https://jsonplaceholder.typicode.com/users";
+  //api: string = "https://jsonplaceholder.typicode.com/users";
+  api: string = "https://jsonplaceholder.typicode.com/posts";
 
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
     // Se devuelven los usuarios de la API como un observable del tipo User
     return this.http.get<User[]>(this.api)
+  }
+
+  getPosts(): Observable<Post[]> {
+    // Se devuelven los posts de la API como un observable del tipo Post
+    return this.http.get<Post[]>(this.api)
   }
 }
